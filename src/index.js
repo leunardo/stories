662abdf6d5c38add98967f17826f9c3644c2f397
Leonardo de Melo Alves <leunardosevla@gmail.com>
2019-05-04 23:56:43 -0300

    feat: add firebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import './tailwind.css';
-import * as serviceWorker from './serviceWorker';
-
 import MainRouter from './router/router';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKHyIPftWsO4DyHx7DntMSB1cYnil2We8",
+    authDomain: "stories-leonardoalves.firebaseapp.com",
+    databaseURL: "https://stories-leonardoalves.firebaseio.com",
+    projectId: "stories-leonardoalves",
+    storageBucket: "stories-leonardoalves.appspot.com",
+    messagingSenderId: "1069643993187",
+    appId: "1:1069643993187:web:1d1f18be34f068ca"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<MainRouter />, document.getElementById('root'));
 
@@ -12,3 +24,8 @@ ReactDOM.render(<MainRouter />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// const db = firebase.firestore();
+// db.collection('stories').get().then((snapshot) => {
+//     snapshot.forEach(console.log);
+// });
