7f369b7579b46a13187332a7c1b84246d5a99f68
Leonardo de Melo Alves <leunardosevla@gmail.com>
2019-05-06 23:13:34 -0300

    feat: fetch stories into homepage

--- src/modules/home/home.page.js
+++ src/modules/home/home.page.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import CardComponent from '../../components/card/card.component';
+import StoriesService from '../../services/stories.service';
 
 export default class HomePage extends React.Component {
+  storiesService = new StoriesService();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stories: []
+    };
+
+    this.stories$ = this.storiesService.stories$.subscribe(stories => {
+      this.setState({ stories });
+    })
+  }
+
   render() {
-    return <div>Hello world</div>;
+    return <div className="flex flex-col min:p-2">
+        { this.state.stories.map((stories, i) => <CardComponent key={i} {...stories} />) }
+    </div>;
+  }
+
+  componentDidMount() {
+    this.storiesService.load();
+  }
+
+  componentWillUnmount() {
+    this.storiesService.dispose();
+    this.stories$.unsubscribe();
   }
 }
